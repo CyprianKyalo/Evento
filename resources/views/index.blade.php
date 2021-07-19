e08fbeed6e99972843eee2c9566914194084efcb
Cyprian Kyalo <cypkyalo17@gmail.com>
2021-07-19 09:47:53 +0300

    Index Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -14,11 +18,84 @@
 
         <link rel="stylesheet" href="{{asset('css/app.css')}}" rel="stylesheet">
 
-        <div class="container">
-        	<h1 class="">Evento</h1>
-        	<h2>The best event management platform</h2>
+        <style type="text/css">
+        	.custom-class {
+        		align-items: center;
+        		justify-content: center;
+        	}
+        </style>
+    </head>
+
+    <body>
+        <div class="container-fluid">
+        	<div class="row border border-dark">
+        		<div class="col border p-5">
+        			<div>
+        				<h1 class="">Evento</h1>
+        			</div>
+        			<div class="mt-5">
+        				<h2 class="">The best event management platform</h2>
+        			</div>
+
+        			
+        		</div>
+
+	        	<div class="col border">
+	        		{{-- Register --}}
+	        		{{-- @include('auth.register') --}}
+        		</div>
+        	</div>
+
+        	<div class="container mt-5">
+        		<p>
+        			Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Dolorem architecto, blanditiis, provident omnis minima eveniet impedit magni ipsam. Minus accusantium esse exercitationem iusto excepturi quia aspernatur aut unde illum sunt.
+        		</p>
+        	</div>
+
+        	<div class="card-deck mt-5">
+        		<div class="card" style="max-width: 270px">
+        			<img src="{{'assets/event1.jpg'}}" alt="" class="card-img" style="height: 300px; width: 270px">
+        		</div>
+
+        		<div class="card" style="max-width: 270px">
+        			<img src="{{'assets/event2.jpg'}}" alt="" class="card-img" style="height: 300px; width: 270px">
+        		</div>
+
+        		<div class="card" style="max-width: 270px">
+        			<img src="{{'assets/event3.jpg'}}" alt="" class="card-img" style="height: 300px; width: 270px">
+        		</div>
+        		
+        	</div>
+
+        	<div class="container-fluid mt-5">
+        		<div class="row align-items-start ">
+        			<div class="col">
+        				<img src="{{'assets/event7.jpg'}}" alt="" class="" style="max-width: 95%">
+        			</div>
+        			<div class="col">
+        				<p>
+        					Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa ratione recusandae voluptatibus quis eligendi dolorum fugit odit numquam porro sit sint exercitationem aliquid omnis et, eos. Dolor, reiciendis non esse!
+        				</p>
+        				
+        			</div>
+        		</div>
+        	</div>
+
+        	<div class="container-fluid mt-5">
+        		<div class="row align-items-start">
+        			<div class="col">
+        				Lorem, ipsum dolor sit amet consectetur, adipisicing elit. Quas quos voluptatibus delectus. Maiores deserunt inventore possimus facere reprehenderit corporis nulla, hic eligendi fuga rerum sunt exercitationem enim assumenda consequatur sequi!
+        			</div>
+        			<div class="col">
+        				<img src="{{'assets/event6.jpg'}}" alt="" class="" style="max-width: 95%">
+        			</div>
+        		</div>
+        	</div>
+    
         </div>
+        
        
         
     </body>
 </html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,38 +15,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //Verifying Email
 Route::get('/verify', [App\Http\Controllers\Auth\RegisterController::class, 'verifyUser'])->name('verify.user');
 
-//Route::get('/test-email', [App\Http\Controllers\MailController::class, 'sendSignupEmail'])->name('test-email');
+//Preventing back history
+Route::group(['middleware' => 'prevent-back-history'], function () {
+	Auth::routes();
 
-//Email Verification notice
-Route::get('/email/verify', function () {
-    return view('auth.verify ');
-})->middleware('auth')->name('verification.notice');
+	Route::get('/', function () {
+    	return view('index');
+	});
 
-//Email Verification Handler
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-//Resending the Verification Email
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
-Route::get('/home', function () {
-    // Only verified users may access this route...
-})->middleware('verified');
+	Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
