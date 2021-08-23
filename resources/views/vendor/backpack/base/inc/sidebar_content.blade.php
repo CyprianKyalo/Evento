59bb979093c5e5ffd2c5002fe022dfc199095b2b
Cyprian Kyalo <cypkyalo17@gmail.com>
2021-08-23 20:11:55 +0300

    File Manager Added

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -17,3 +17,4 @@
 
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('charts') }}'><i class='nav-icon la la-signal'></i> Charts</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('log') }}'><i class='nav-icon la la-terminal'></i> Logs</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}"><i class="nav-icon la la-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
